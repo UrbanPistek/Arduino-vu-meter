890c029bc993cd22ac8552928c56dfe8427a1534
UrbanPistek <urban.pistek@gmail.com>
2019-07-03 19:10:25 -0400

    Low Pass Filter

--- Vu Meter/Vu_Meter_June/Vu_Meter_June.ino
+++ Vu Meter/Vu_Meter_June/Vu_Meter_June.ino
@@ -1,41 +1,71 @@
-//Make this into an array 
- /*int LED_PIN1 = 2;
- int LED_PIN2 = 3;
- int LED_PIN3 = 4; 
- int LED_PIN4 = 5;
- int LED_PIN5 = 6;
- int LED_PIN6 = 7;
- int LED_PIN7 = 8;
- int LED_PIN8 = 9;
- int LED_PIN9 = 10;*/
- int LED_PINS[] = {2,3,4,5,6,7,8,9};
- int Micro_IN = A0; 
- int microphone_value = 0;
+const int SIZE = 10; 
+//Experiment with using pointers instead later 
+class RunningAverage{
+  int data[SIZE] = {0}; 
+  int index = 0; 
+public: 
+  void add_element(int element){
+    data[index] = element; 
+    index++; 
+    index = index%SIZE; 
+  }
+
+  int average(){
+    int sum = 0; 
+    for(int idx = 0; idx < SIZE; idx++){
+      sum = sum + data[idx]; 
+    }
+    return sum/SIZE;
+  }
+};
+/*
+  int data[SIZE]; 
+  int index = 0; 
   
-void setup() {
+  void add_element(int element){
+    data[index] = element; 
+    index++; 
+    index = index%SIZE; 
+  }
+
+  int average(int data[]){
+    int sum = 0; 
+    for(int idx = 0; idx < SIZE; idx++){
+      sum = sum + data[idx]; 
+    }
+    return sum/SIZE; 
+  }*/
+  
+RunningAverage Av;
+int LED_PINS[] = {2,3,4,5,6,7,8,9};
+int Micro_IN = A0; 
+int microphone_value = 0;
+  
+void setup(){
 Serial.begin(9600);
   for(int idx = 0; idx < 8; idx++){
     pinMode(LED_PINS[idx], OUTPUT);  
   }
   pinMode(Micro_IN, INPUT);
-  analogReference(INTERNAL);  
+  analogReference(INTERNAL); 
 }
 
-void loop() {
-  microphone_value = analogRead(Micro_IN); 
-  Serial.println(microphone_value);
-    if(microphone_value > 275 || microphone_value < 270)
-     digitalWrite(LED_PINS[0], HIGH);
-    else 
-     digitalWrite(LED_PINS[0], LOW);
-  /*
-  digitalWrite(LED_PIN1, HIGH); 
-  digitalWrite(LED_PIN4, HIGH);
-  digitalWrite(LED_PIN7, HIGH);
-  delay(500); 
-  digitalWrite(LED_PIN1, LOW); 
-  digitalWrite(LED_PIN4, LOW);
-  digitalWrite(LED_PIN7, LOW);
-  delay(500);
- */
+void loop(){
+       Av.add_element(analogRead(Micro_IN)); 
+       Serial.println(Av.average());
+       /*
+       add_element(analogRead(Micro_IN)); 
+       Serial.println(average(data)); 
+       */
 }
+
+/*
+ Why you can't take a foureir transform and determine the frquancies of the noise:
+ Becasue doing the foureir transform and them converting it back to the time domain can take too much time
+ It can be done in real time (using a butterfly algorithm), however you need to record all the
+ data up to that point (ie. define a window) 
+ */
+
+ /*
+  * 
+  */
