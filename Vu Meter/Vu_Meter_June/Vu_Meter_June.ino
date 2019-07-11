6f23eeb33fc8f6df6f9b21e1b711ddb151bfd4d1
UrbanPistek <urban.pistek@gmail.com>
2019-07-10 20:42:12 -0400

    Completed Working Code

--- Vu Meter/Vu_Meter_June/Vu_Meter_June.ino
+++ Vu Meter/Vu_Meter_June/Vu_Meter_June.ino
@@ -1,4 +1,12 @@
 const int SIZE = 10; 
+const int MAX = 30;
+const int MIN = 0; 
+const int RANGE = MAX - MIN; 
+int LED_PINS[] = {2,3,4,5,6,7,8,9,10};
+int Micro_IN = A0; 
+int microphone_value = 0;
+int vol = 0;
+
 //Experiment with using pointers instead later 
 class RunningAverage{
   int data[SIZE] = {0}; 
@@ -18,28 +26,31 @@ public:
     return sum/SIZE;
   }
 };
-/*
-  int data[SIZE]; 
-  int index = 0; 
-  
-  void add_element(int element){
-    data[index] = element; 
-    index++; 
-    index = index%SIZE; 
-  }
 
-  int average(int data[]){
-    int sum = 0; 
-    for(int idx = 0; idx < SIZE; idx++){
-      sum = sum + data[idx]; 
-    }
-    return sum/SIZE; 
-  }*/
+int read_background_noise(){
+  int sum = 0; 
+  for(int idx= 0; idx < 10; idx++){
+    sum += analogRead(Micro_IN); 
+  }
+  return sum/10; 
+}
+
+void LED_range(int vol){
+  int bucket_size = RANGE/10; 
+  int magitude = abs(vol/bucket_size); 
+  
+  for(int idx = 0; idx < 9; idx++){
+    digitalWrite(LED_PINS[idx], LOW); 
+   } 
+   
+  //if(abs(vol) > MIN){
+  for(int idx = 0; idx < magitude; idx++){
+    digitalWrite(LED_PINS[8-idx], HIGH); 
+   }
+  //}
+}
   
 RunningAverage Av;
-int LED_PINS[] = {2,3,4,5,6,7,8,9};
-int Micro_IN = A0; 
-int microphone_value = 0;
   
 void setup(){
 Serial.begin(9600);
@@ -47,16 +58,16 @@ Serial.begin(9600);
     pinMode(LED_PINS[idx], OUTPUT);  
   }
   pinMode(Micro_IN, INPUT);
-  analogReference(INTERNAL); 
+  analogReference(INTERNAL);
+   
 }
 
-void loop(){
+void loop(){ 
        Av.add_element(analogRead(Micro_IN)); 
-       Serial.println(Av.average());
-       /*
-       add_element(analogRead(Micro_IN)); 
-       Serial.println(average(data)); 
-       */
+       vol = Av.average() - read_background_noise();
+       LED_range(vol);  
+       Serial.println(vol); 
+       
 }
 
 /*
